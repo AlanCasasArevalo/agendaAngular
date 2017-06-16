b61ba7a62fba5add4576e66d59775b07d1e5612a
Alan Casas <alancasas@MacBook-Pro-de-Alan.local>
2017-06-16 17:23:59 +0200

    Cambio de template app.component.html eliminando el template creado por cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+/*
+Se puede poner o bien como arriba o bien como abajo, sin embaargo lo mas normal es hacerlo como la parte de arriba.
 import { Component } from '@angular/core';
-
+import { OnInit } from "@angular/core"
+*/
 @Component({
+  // Indicamos el selector que recoge del html. Es una etiqueta que nosotros nos inventamos y luego la metemos en el index.html.
   selector: 'app-root',
+  // Indicamos la ruta al template del componente, se puede hacer todo el HTML en una linea pero no es buena idea. 
   templateUrl: './app.component.html',
+  // Indicamos la ruta al CSS del elemento, se puede tener mas de un CSS. Se puede crear el CSS en sin hacer rutas pero es una mala idea
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+
+export class AppComponent implements OnInit {
   title = 'Aplicacion Angular';
+
+  ngOnInit(){
+    console.log("Estoy vivo todavia")
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // Indicamos los componentes pipes y directivas de la app
   declarations: [
     AppComponent
   ],
+  // Importamos modulos que necesita la aplicacion
   imports: [
     BrowserModule
   ],
+  // Indicamos los proveedores de las piezas que sean susceptibles de que sean inyectadas como dependencias
   providers: [],
+  // Es el modulo raiz de la aplicacion que es con la que arranca la aplicacion. 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
